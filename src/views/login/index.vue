e84a71f750ccbe064dd020520d9d929c99bd0241
luxintao <15920824726@163.com>
2020-08-25 16:58:18 +0800

    尼玛 git reset不管用

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -8,10 +8,6 @@
     export default {
         name: "index",
         created() {
-            console.log(1)
-            console.log(2)
-            console.log(3)
-            console.log(4)
             console.log('views/login/index edwin')
         },
         methods: {
